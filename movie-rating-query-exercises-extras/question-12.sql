9b41d7b342d6a67770bfa4b7f02f552ebb47a70b
jasminehilton <jasminehilton1@live.com>
2023-05-02 10:13:32 -0700

    Question 12 solved. Only works on mysql lite

--- movie-rating-query-exercises-extras/question-12.sql
+++ movie-rating-query-exercises-extras/question-12.sql
@@ -2,3 +2,9 @@
 -- For each director, return the director's name together with the title(s) of the movie(s) they directed that received the highest rating among all of their movies, and the value of that rating. Ignore movies whose director is NULL.
 
 -- Answer #12
+SELECT Movie.director, Movie.title, MAX(Rating.stars)
+FROM Movie 
+JOIN Rating 
+ON Rating.mID = Movie.mID
+GROUP BY Movie.director
+HAVING Movie.director IS NOT NULL;
