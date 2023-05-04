dff64fd1d15a629d8e5f79e094d508ef9f4cb9ef
jasminehilton <jasminehilton1@live.com>
2023-05-04 12:17:54 -0700

    question 3 tbt

---
+++ social-network-query-excercises/question-3.sql
@@ -0,0 +1,5 @@
+-- Question #3
+-- For every pair of students who both like each other, return the name and grade of both students. Include each pair only once, with the two names in alphabetical order.
+
+-- Answer #3
+-- tbt
