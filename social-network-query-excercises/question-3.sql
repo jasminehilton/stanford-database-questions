3e9c20ad10cb8dcf839b67a3a17efcaf09b63e0b
jasminehilton <jasminehilton1@live.com>
2023-05-05 08:11:51 -0700

    question 3 solved

--- social-network-query-excercises/question-3.sql
+++ social-network-query-excercises/question-3.sql
@@ -2,4 +2,13 @@
 -- For every pair of students who both like each other, return the name and grade of both students. Include each pair only once, with the two names in alphabetical order.
 
 -- Answer #3
--- tbt
+SELECT h1.name, h1.grade, h2.name, h2.grade
+FROM Likes l1
+JOIN Likes l2
+ON l1.ID1 = l2.ID2
+AND l1.ID2 = l2.ID1
+JOIN Highschooler h1
+ON h1.ID = l1.ID1
+JOIN Highschooler h2
+ON h2.ID = l1.ID2
+WHERE h1.name < h2.name
