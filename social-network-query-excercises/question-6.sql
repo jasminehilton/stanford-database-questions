5ba24b5710793c335dfc57b304be5cbf67ef4760
jasminehilton <jasminehilton1@live.com>
2023-05-08 14:48:59 -0700

    tbt

---
+++ social-network-query-excercises/question-6.sql
@@ -0,0 +1,5 @@
+-- Question #6
+-- Find names and grades of students who only have friends in the same grade. Return the result sorted by grade, then by name within each grade.
+
+-- Answer #6
+-- tbt
---
+++ social-network-query-excercises/question-9.sql
@@ -0,0 +1,4 @@
+-- Question #9
+-- Find the name and grade of all students who are liked by more than one other student.
+
+-- Answer #9
