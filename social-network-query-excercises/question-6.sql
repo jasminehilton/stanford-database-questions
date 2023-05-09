0f960d6a4b600fd4ec70bb09c41146c88a727d1b
jasminehilton <jasminehilton1@live.com>
2023-05-09 11:27:26 -0700

    question 6 solved

--- social-network-query-excercises/question-6.sql
+++ social-network-query-excercises/question-6.sql
@@ -2,4 +2,19 @@
 -- Find names and grades of students who only have friends in the same grade. Return the result sorted by grade, then by name within each grade.
 
 -- Answer #6
--- tbt
+SELECT DISTINCT h1.name, h1.grade
+FROM Friend
+JOIN Highschooler h1
+ON h1.ID = Friend.ID1
+JOIN Highschooler h2
+ON h2.ID = Friend.ID2
+WHERE h1.grade = h2.grade
+AND h1.ID NOT IN 
+(SELECT Friend.ID1
+FROM Friend
+JOIN Highschooler h3
+ON h3.ID = Friend.ID1
+JOIN Highschooler h4
+ON h4.ID = Friend.ID2
+WHERE h3.grade <> h4.grade)
+ORDER BY h1.grade, h1.name
