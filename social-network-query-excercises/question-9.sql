acb64d5f148d2f5b6a3ec25aeb406571b3eaeb0b
jasminehilton <jasminehilton1@live.com>
2023-05-09 14:54:30 -0700

    question 9 solved

--- social-network-query-excercises/question-9.sql
+++ social-network-query-excercises/question-9.sql
@@ -2,3 +2,11 @@
 -- Find the name and grade of all students who are liked by more than one other student.
 
 -- Answer #9
+SELECT h2.name, h2.grade
+FROM Likes
+JOIN Highschooler h1 
+ON h1.ID = Likes.ID1
+JOIN Highschooler h2
+ON h2.ID = Likes.ID2
+GROUP BY h2.name, h2.grade
+HAVING COUNT(h2.name) > 1
