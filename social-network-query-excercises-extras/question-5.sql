bcb10b1740a1ca40d72c52f76e9e0999d19c084c
jasminehilton <jasminehilton1@live.com>
2023-05-10 15:24:36 -0700

    question 5 solved

--- social-network-query-excercises-extras/question-5.sql
+++ social-network-query-excercises-extras/question-5.sql
@@ -1,8 +1,16 @@
--- Question #1
--- 
+-- Question #5
+-- Find the name and grade of the student(s) with the greatest number of friends.
 
--- Answer #1
-
+-- Answer #5
+SELECT name, grade
+	FROM Friend
+    JOIN Highschooler on Highschooler.ID = Friend.ID1
+	GROUP BY Friend.ID1, name, grade
+    Having COUNT(Friend.ID1) >= (SELECT MAX(count) as maximum
+FROM
+	(SELECT COUNT(Friend.ID1) AS count, ID1
+	FROM Friend
+	GROUP BY Friend.ID1) AS fvredg)
 
 
 
