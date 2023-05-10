dbab7e5ab223e202dbff4eac382adb18904eb8b0
jasminehilton <jasminehilton1@live.com>
2023-05-10 13:35:42 -0700

    question 3 solved

--- social-network-query-excercises-extras/question-3.sql
+++ social-network-query-excercises-extras/question-3.sql
@@ -1,4 +1,10 @@
 -- Question #3
---
+-- What is the average number of friends per student? (Your result should be just one number.)
 
 -- Answer #3
+SELECT AVG(count)
+FROM (SELECT COUNT(Friend.ID1) AS count
+FROM Highschooler
+JOIN Friend
+ON Friend.ID1 = Highschooler.ID
+GROUP BY Friend.ID1) AS countTable
