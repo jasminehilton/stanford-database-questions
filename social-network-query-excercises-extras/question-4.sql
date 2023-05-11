552717a1a8acfa28db7bea88ca616b3e2d9af05d
jasminehilton <jasminehilton1@live.com>
2023-05-11 12:20:49 -0700

    question 4 solved

--- social-network-query-excercises-extras/question-4.sql
+++ social-network-query-excercises-extras/question-4.sql
@@ -1,4 +1,17 @@
 -- Question #4
--- 
+-- Find the number of students who are either friends with Cassandra or are friends of friends of Cassandra. Do not count Cassandra, even though technically she is a friend of a friend.
 
 -- Answer #4
+SELECT COUNT(*)
+FROM (SELECT DISTINCT f1.ID2
+FROM Friend f1
+WHERE f1.ID1 = 1709
+UNION 
+SELECT DISTINCT f2.ID2
+FROM Friend f1 , Friend f2
+WHERE f1.ID1 = 1709
+AND f2.ID1 = f1.ID2
+AND f2.ID2 <> 1709) AS countTable
+
+
+
