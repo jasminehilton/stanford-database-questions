181bc8115305f391bddd4ceb2ff654bdf00a18db
jasminehilton <jasminehilton1@live.com>
2023-05-15 17:11:16 -0700

    question 2 solved

--- movie-rating-modification-exercises/question-2.sql
+++ movie-rating-modification-exercises/question-2.sql
@@ -2,3 +2,11 @@
 -- For all movies that have an average rating of 4 stars or higher, add 25 to the release year. (Update the existing tuples; don't insert new tuples.)
 
 -- Answer #2
+
+UPDATE Movie
+SET year = year + 25
+WHERE Movie.mID IN
+(SELECT Rating.mID
+FROM Rating  
+GROUP BY Rating.mID
+HAVING AVG(stars) >= 4) ;
