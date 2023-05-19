543341c4ce6427a2e7c5b6d34102cc7cc853e8d0
jasminehilton <jasminehilton1@live.com>
2023-05-19 11:13:21 -0700

    solved

--- movie-rating-modification-exercises/question-3.sql
+++ movie-rating-modification-exercises/question-3.sql
@@ -2,3 +2,7 @@
 -- Remove all ratings where the movie's year is before 1970 or after 2000, and the rating is fewer than 4 stars.
 
 -- Answer #3
+DELETE from Rating
+where ( 
+(select year from Movie where Movie.mID = Rating.mID) not between 1970 and 2000) 
+and stars < 4;
